1c344ed7a2baaaeb25edf3a978b423752c761a2e
mayurch25 <mayurch25@gmail.com>
2026-04-07 13:16:00 +0530

    refactor: update server listening logic and add console log for server status

--- index.js
+++ index.js
@@ -59,4 +59,7 @@ app.post("/edit", function (req, res) {
   );
 });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function () {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
